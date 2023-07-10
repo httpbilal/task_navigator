19931f502341e2cad1487b7e58420860c9443c81
bilalbug <malikbilaljavaid@gmail.com>
2023-07-10 17:40:23 +0500

    All necessary models, controllers and seeders are added successfully!

---
+++ app/Http/Controllers/WorkSpaceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\WorkSpace;
+use Illuminate\Http\Request;
+
+class WorkSpaceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\WorkSpace  $workSpace
+     * @return \Illuminate\Http\Response
+     */
+    public function show(WorkSpace $workSpace)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\WorkSpace  $workSpace
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(WorkSpace $workSpace)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\WorkSpace  $workSpace
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, WorkSpace $workSpace)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\WorkSpace  $workSpace
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(WorkSpace $workSpace)
+    {
+        //
+    }
+}
